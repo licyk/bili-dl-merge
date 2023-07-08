06f6fb351fd5482f5574c66d54b30909f077162b
licyk <76895225+licyk@users.noreply.github.com>
2023-07-09 01:49:03 +0800

    Add files via upload

--- bili-dl-merge.sh
+++ bili-dl-merge.sh
@@ -10,13 +10,13 @@ function filemanager()
 
 	if [ $(pwd) = "/" ];then
 		file_select=$(dialog --clear --title "文件管理" \
-			--menu "使用方向键和回车键进行选择\n名称后面带有\"/\"的是文件夹\n文件和文件夹按时间排序,最新的排在前面\n当前路径$(pwd)" 20 60 10 \
+			--menu "使用方向键和回车键进行选择\n名称后面带有\"/\"的是文件夹\n文件和文件夹按时间排序,最新的排在前面\n当前路径$(pwd)" 25 60 10 \
 				$dir_list \
 				3>&1 1>&2 2>&3)
 
 	else
 		file_select=$(dialog --clear --title "文件管理" \
-			--menu "使用方向键和回车键进行选择\n名称后面带有\"/\"的是文件夹\n文件和文件夹按时间排序,最新的排在前面\n当前路径$(pwd)" 20 60 10 \
+			--menu "使用方向键和回车键进行选择\n名称后面带有\"/\"的是文件夹\n文件和文件夹按时间排序,最新的排在前面\n当前路径$(pwd)" 25 60 10 \
 				"-->返回上一个目录" "X" \
 				$dir_list \
 				3>&1 1>&2 2>&3)
@@ -36,7 +36,7 @@ function filemanager()
 				process_methon
 				filemanager
 			else
-				dialog --clear --title "文件管理" --msgbox "文件格式错误,请选择以m4s后缀的文件" 20 60
+				dialog --clear --title "文件管理" --msgbox "文件格式错误,请选择以m4s后缀的文件" 25 60
 				filemanager
 			fi
 		else 
@@ -44,7 +44,7 @@ function filemanager()
 				cd ..
 				filemanager
 			else
-				dialog --clear --title "文件管理" --msgbox "打开路径失败,可能是文件夹名称包含空格,或者是文件名包括空格,软件认为该文件为文件夹" 20 60
+				dialog --clear --title "文件管理" --msgbox "打开路径失败,可能是文件夹名称包含空格,或者是文件名包括空格,软件认为该文件为文件夹" 25 60
 				filemanager
 			fi
 		fi
@@ -81,15 +81,28 @@ function start_option()
 		unset output_path
 	fi
 
-	if (dialog --clear --title "启动选项" --yes-label "启动文件管理" --no-label "设置启动路径" --yesno "请选择要进行的操作\n第一次使用推荐设置启动路径\n启动文件管理后寻找后缀为\"m4s\"的文件即可开始转换\n如果要退出该脚本,先启动文件管理,然后会出现一个取消按钮用于退出\n\n当前配置文件状态:"$config_status"\n启动路径:\n"$_setup_path"\n保存路径:\n"$_output_path"\n\n使用方向键,Tab键,回车键进行选择" 20 60) then #选择启动文件管理
-		if [ -e .bili_dl_merge.ini ];then
-			final_setup_path=$(cat "$script_start_path"/.bili_dl_merge.ini | awk 'NR==3' )
-			cd $final_setup_path
+	start_option_=$(dialog --clear --title "启动选项" --menu "请选择要进行的操作\n第一次使用推荐设置启动路径\n启动文件管理后寻找后缀为\"m4s\"的文件即可开始转换\n\n当前配置文件状态:"$config_status"\n启动路径:\n"$_setup_path"\n保存路径:\n"$_output_path"\n\n使用方向键,Tab键,回车键进行选择" 25 60 10 \
+			"1" "启动文件管理" \
+			"2" "设置启动路径" \
+			"3" "更新脚本" \
+			"4" "退出" \
+			3>&1 1>&2 2>&3)
+	if [ $? = 0 ];then
+		if [ $start_option_ = "1" ];then #选择启动文件管理
+			if [ -e .bili_dl_merge.ini ];then
+				final_setup_path=$(cat "$script_start_path"/.bili_dl_merge.ini | awk 'NR==3' )
+				cd $final_setup_path
+			fi
+			filemanager
+		elif [ $start_option_ = "2" ];then #选择设置启动路径
+			setup_path
+		elif [ $start_option_ = "3" ];then
+			update_option
+		elif [ $start_option_ = "4" ];then
+			exit
 		fi
-		filemanager
-	else #选择设置启动路径
-		setup_path
 	fi
+	exit
 }
 
 #设置路径选项
@@ -97,7 +110,7 @@ function setup_path()
 {
 	if (dialog --clear --title "路径选项" --yes-label "是" --no-label "否" --yesno "是否更改路径\n选择\"是\"将会清除当前的路径配置\n路径配置为.bili_dl_merge.ini,在脚本所在目录中产生，可手动编辑或删除" 20 60) then
 	#设置启动路径
-	final_setup_path=$(dialog --clear --title "启动路径设置" --inputbox "请输入哔哩哔哩视频下载的目录\n哔哩哔哩目录在Android/data\n目录的格式例子：\n/storage/emulated/0/Android/data/tv.danmaku.bili/download" 20 60 3>&1 1>&2 2>&3)
+	final_setup_path=$(dialog --clear --title "启动路径设置" --inputbox "请输入哔哩哔哩视频下载的目录\n哔哩哔哩目录在Android/data\n目录的格式例子：\n/storage/emulated/0/Android/data/tv.danmaku.bili/download" 25 60 3>&1 1>&2 2>&3)
 	setup_path_status=$?
 	if [ $setup_path_status = 0 ];then
 		cd $script_start_path 
@@ -112,7 +125,7 @@ function setup_path()
 	fi
 
 	#设置保存路径
-	final_output_path=$(dialog --clear --title "转换文件保存路径" --inputbox "请输入转换文件的保存路径\n不设置时默认在选择转换的文件的目录下保存\n目录的格式例子\n/storage/emulated/0/Downloads" 20 60 3>&1 1>&2 2>&3 )
+	final_output_path=$(dialog --clear --title "转换文件保存路径" --inputbox "请输入转换文件的保存路径\n不设置时默认在选择转换的文件的目录下保存\n目录的格式例子\n/storage/emulated/0/Downloads" 25 60 3>&1 1>&2 2>&3 )
 	setup_path_status_=$?
 	if [ $setup_path_status_ = 0 ];then
 		echo "下面为转换文件的保存路径" >> .bili_dl_merge.ini
@@ -126,6 +139,11 @@ function setup_path()
 else
 	start_option
 fi
+}
+
+function update()
+{
+	d
 }
 
 #################################################
